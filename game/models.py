d1bc8593714f3ebed9d687e435372f3f57e8f140
JanetAdedosu <adedosuadeola71@gmail.com>
2026-03-24 19:41:47 +0100

    storage for round one

--- game/models.py
+++ game/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class GameSession(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    cash = models.IntegerField()
+
+    financial = models.FloatField()
+    human = models.FloatField()
+    green = models.FloatField()
+    resilience = models.FloatField()
+    digital = models.FloatField()
+
+
+class Decision(models.Model):
+    game = models.ForeignKey(GameSession, on_delete=models.CASCADE)
+    round_number = models.IntegerField()
+    choice = models.CharField(max_length=1)
+
+    R = models.FloatField()
+    E = models.FloatField()
+    S = models.FloatField()
+    B = models.FloatField()
+    T = models.FloatField()
+    P = models.FloatField()
+    A = models.FloatField()
