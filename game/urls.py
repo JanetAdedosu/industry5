b47a710b46fb3571c2bac265e14142312e0d40b1
JanetAdedosu <adedosuadeola71@gmail.com>
2026-03-25 09:15:54 +0100

    process decision

--- game/urls.py
+++ game/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import game_view
+from .views import game_view, process_decision
 
 urlpatterns = [
     path('', game_view, name='game'),
+    path('process/', process_decision),
 ]
